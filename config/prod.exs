ba99c28461678aebb39f697b0707d49a650e7d9e
Jon Rowe <hello@jonrowe.co.uk>
2018-03-28 14:55:26 +1100

    Add prod basic config

--- config/prod.exs
+++ config/prod.exs
@@ -6,3 +6,10 @@ config :minibase, MinibaseWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 config :logger, level: :info
+
+# Note this works for heroku, as it compiles on deploy
+config :minibase, Minibase.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  hostname: "localhost",
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 10
