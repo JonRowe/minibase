f2a5752f3ccaa5cc575c3f5e731eb07d0c96c73a
Jon Rowe <hello@jonrowe.co.uk>
2021-02-27 09:24:14 +0000

    Use runtime config

---
+++ config/runtime.exs
@@ -0,0 +1,13 @@
+import Config
+
+config :minibase, MinibaseWeb.Endpoint,
+  http: [port: System.get_env("PORT")],
+  url: [host: "localhost"],
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  render_errors: [view: MinibaseWeb.ErrorView, accepts: ~w(html json)],
+  pubsub_server: Minibase.PubSub
+
+# Note this works for dev and heroku, as it compiles on deploy
+config :minibase, Minibase.Repo,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 10
